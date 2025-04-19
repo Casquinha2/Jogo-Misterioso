a0cb0bbd3534982e822c4f89b8c2e39403571581
Casquinha2 <tmdscasquinha@gmail.com>
2025-04-19 18:37:32 +0100

    andar da personagem com animacoes
    as animacoes estao incompletas, e so necessario mudar as sprites

---
+++ Jogo Misterioso/Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; // Import the Input System namespace
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f; // Speed of the player
+    private Rigidbody2D rb; // Reference to the Rigidbody2D component
+    private Vector2 moveInput; // Store the movement input
+    private Animator animator;
+    private float afktimer = 0f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the player
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed; // Set the velocity of the Rigidbody2D based on input and speed
+        
+        if(afktimer >= 5f)
+        {
+            animator.SetBool("isAFK", true);
+        }
+
+        afktimer += Time.deltaTime;
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+        animator.SetBool("isAFK", false);
+        
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+
+            
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+        afktimer = 0f;
+    }
+
+}
