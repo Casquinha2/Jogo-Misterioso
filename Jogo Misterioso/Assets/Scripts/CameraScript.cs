e74f5aaa5caf74546d11910e90735f67f1547ea1
Pedrovic1970 <senseiandr@gmail.com>
2025-06-09 20:14:44 +0100

    update 2

--- Jogo Misterioso/Assets/Scripts/CameraScript.cs
+++ Jogo Misterioso/Assets/Scripts/CameraScript.cs
@@ -4,12 +4,14 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject mainCam;
     public GameObject lightCamera;
+    public GameObject Room62Collider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DontDestroyOnLoad(gameObject);
         DontDestroyOnLoad(mainCam);
         DontDestroyOnLoad(lightCamera);
+        DontDestroyOnLoad(Room62Collider);
     }
 
     // Update is called once per frame
